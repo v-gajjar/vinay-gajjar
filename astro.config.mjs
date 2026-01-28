fe4aea4b04bb4a3bd328834bef0b01b5c14c2bd4
vgajjar <vgajjar22@gmail.com>
2026-01-28 10:23:47 +0000

    first attempt at replacing inline SVG markup with Asto Icon components, using Bootstrap Icons via Iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        bi: ["*"], // (Default) Loads entire Bootstrap Icon set
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   },
